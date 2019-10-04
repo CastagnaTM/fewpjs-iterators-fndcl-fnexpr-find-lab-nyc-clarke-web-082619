ed3601ed672645248e0173d95d4587df89f9772b
castagnaTM <castagna.tm@gmail.com>
2019-10-04 16:35:31 -0400

    Done.

--- index.js
+++ index.js
@@ -3,3 +3,8 @@ const testVar = {}
 function testFunc() {
   return "hi"
 }
+
+function superbowlWin(record) {
+  let a = record.find( record => record.result === "W" )
+	return a ? a.year : undefined
+}
